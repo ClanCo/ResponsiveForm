7f40134efdafd15b96d24c02b270ee4eaee42eda
CorentinT <corentin.terrie@gmail.com>
2017-02-25 12:55:07 +0100

    Validation of the data

--- index.php
+++ index.php
@@ -1,12 +1,38 @@
 <?php
     $firstname = $name = $phone = $message = $email = "";
+    $firstname_error = $name_error = $phone_error = $message_error = $email_error = "";
+    
     if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
+        //security check on post $var
         $firstname = verifyInput($_POST['firstname']);
         $name = verifyInput($_POST['name']);
         $phone = verifyInput($_POST['phone']);
         $message = verifyInput($_POST['message']);
         $email = verifyInput($_POST['email']);
+        
+        if(empty($firstname))
+        {
+            $firstname_error = "Le prénom est requis.";
+        }
+        if(empty($name))
+        {
+            $name_error = "Le nom est requis.";
+        }
+        if(empty($message))
+        {
+            $message_error = "Pas de message pas de chocolat!";
+        }
+        if(!isEmail($email))
+        {
+            $email_error = "Ca ressemble bof bof à un email ça!";
+        }
+        if(!isPhone($phone))
+        {
+            $phone_error = "Entrez un numéro avec seulement chiffres et espaces.";
+        }
+
+        
     }
 
     function verifyInput($var)
@@ -16,7 +42,16 @@
         $var = htmlspecialchars($var); // create a string of script and join them to html request
         return $var;
     }
- 
+    function isEmail($var)
+    {
+        return filter_var($var, FILTER_VALIDATE_EMAIL);
+        
+    }
+
+    function isPhone($var)
+    {
+        return preg_match("/^[0-9 ]*$/", $var);
+    }
 ?>
 
 
@@ -50,29 +85,29 @@
                     <form id="contact-form" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" role="form">
                         <div class="col-md-6">
                             <label for="firstname">Prénom<span class="blue"> *</span></label>
-                            <input type="text" name="firstname" id="firstname" class="form-control" placeholder="Votre prénom" value="<?php echo $firstname ;?>">
-                            <p class="comments">Message d'erreur</p>
+                            <input type="text" name="firstname" id="firstname" class="form-control" placeholder="Votre prénom" value="<?php echo $firstname ;?>" >
+                            <p class="comments"><?php echo $firstname_error; ?></p>
                         </div>
                         <div class="col-md-6">
                             <label for="name">Nom<span class="blue"> *</span></label>
-                            <input type="text" name="name" id= "name" class="form-control" placeholder="Votre nom" value="<?php echo $name;?>">
-                            <p class="comments">Message d'erreur</p>
+                            <input type="text" name="name" id= "name" class="form-control" placeholder="Votre nom" value="<?php echo $name;?>" >
+                            <p class="comments"><?php echo $name_error; ?></p>
                         </div>
                         <div class="col-md-6">
                             <label for="email">Email<span class="blue"> *</span></label>
-                            <input type="text" name="email" id="email" class="form-control" placeholder="Votre email" value="<?php echo $email;?>">
-                            <p class="comments">Message d'erreur</p>
+                            <input type="email" name="email" id="email" class="form-control" placeholder="Votre email" value="<?php echo $email; ?>" >
+                            <p class="comments"><?php echo $email_error; ?></p>
                         </div>
                         <div class="col-md-6">
                             <label for="phone">Téléphone</label>
-                            <input type="text" name="phone"  id="phone" class="form-control" placeholder="Votre téléphone" value="<?php echo $phone;?>">
-                            <p class="comments">Message d'erreur</p>
+                            <input type="tel" name="phone"  id="phone" class="form-control" placeholder="Votre téléphone" value="<?php echo $phone;?>">
+                            <p class="comments"><?php echo $phone_error; ?></p>
                         </div>
                         <div class="col-md-12">
                             <label for="message">Message<span class="blue"> *</span></label>
-                            <textarea id="message" name="message" class="form-control" placeholder="Votre message" rows="4"><?php echo $message;?>
+                            <textarea id="message" name="message" class="form-control" placeholder="Votre message" rows="4" ><?php echo $message; ?>
                             </textarea > 
-                            <p class="comments">Message d'erreur</p>
+                            <p class="comments"><?php echo $message_error; ?></p>
                         </div>
                         <div class="col-md-12">
                             <p class="blue"><strong>* Ces informations sont requises</strong></p>
